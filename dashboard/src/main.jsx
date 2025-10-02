d52b74b8ba95f2924cca589f29abf6fba8872621
shubhamsingh7575 <shubham1728pcm@gmail.com>
2025-10-03 04:58:05 +0530

    creating dashbord features

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // ✅ add Routes + Route
+import Home from "./components/Dashboard";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
